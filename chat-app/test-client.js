7dffa88169810173671109007b5826ddebf24387
scottishwidow <mike.voznyanskix@gmail.com>
2023-11-13 16:17:05 +0200

    modified:   chat-app/test-client.js 	modified:   chat-app/test-server.js 	renamed:    sender.py -> test/sender.py 	renamed:    server.py -> test/server.py

--- chat-app/test-client.js
+++ chat-app/test-client.js
@@ -1,7 +1,29 @@
 const net = require("net");
+const readline = require("readline/promises")
 
-const client = net.createConnection({host: "127.0.0.1", port: "3008"}, () => {
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
+
+const socket = net.createConnection(
+    { host: "127.0.0.1", port: "3008" }, 
+    async () => {
     console.log("Connection successful!");
+
+    const message = await rl.question("Enter a message > ");
+
+    socket.write(message);
 });
+
+socket.on("data", (data) => {
+    console.log(data.toString("utf-8"));
+});
+
+
+socket.on("end", () => {
+    console.log("Connection ended!");
+});
+
 
--- chat-app/test-server.js
+++ chat-app/test-server.js
@@ -2,9 +2,19 @@ const net = require("net");
 
 const server = net.createServer();
 
+// an array of client sockets(objects)
+const clients = [];
+
 server.on("connection", (socket) => {
-    
     console.log("A new connection received.");
+
+    socket.on("data", (data) => {
+        clients.map((s) => {
+            s.write(data)
+        });
+    });
+
+    clients.push(socket)
 });
 
 server.listen(3008, "127.0.0.1", () => {
