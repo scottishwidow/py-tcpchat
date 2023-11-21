34d72f5becf735d2d50fe9d4ed728416f34d2cbc
scottishwidow <mike.voznyanskix@gmail.com>
2023-11-21 23:18:19 +0200

    some comments

--- chat-app/test-client.js
+++ chat-app/test-client.js
@@ -4,22 +4,52 @@ const readline = require("readline/promises")
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
+const clearLine = (dir) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.clearLine(dir, () => {
+            resolve();
+        });
+    });
+};
+
+const moveCursor = (dx, dy) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.moveCursor(dx, dy, () => {
+            resolve();
+        });
+    });
+};
 
 const socket = net.createConnection(
-    { host: "127.0.0.1", port: "3008" }, 
+    { host: "127.0.0.1", port: 3008 },
     async () => {
-    console.log("Connection successful!");
+        console.log("Connected to the server!");
 
-    const message = await rl.question("Enter a message > ");
+        const ask = async () => {
+            const message = await rl.question("Enter a message > ");
+            // move the cursor one line up
+            await moveCursor(0, -1);
+            //clear the current line that the cursor is in
+            await clearLine(0);
+            socket.write(message);
+        }; 
 
-    socket.write(message);
-});
+        ask();
 
-socket.on("data", (data) => {
-    console.log(data.toString("utf-8"));
-});
+        socket.on("data", async (data) => {
+            // log an empty line
+            console.log();
+            // move the cursor one line up
+            await moveCursor(0, -1);
+            // clear that line that cursor just moved into
+            await clearLine(0);
+            console.log(data.toString("utf-8"));
+            ask();
+        });
+    }
+);
 
 
 socket.on("end", () => {
